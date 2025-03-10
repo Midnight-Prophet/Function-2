a1ac28c80fde34451cadf494be7d63ec3c8a5422
Isaac <isaac@Isaacs-iMac.local>
2025-03-10 11:09:17 -0400

    Updated Code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function numberofVowels(str) {
     let words = str.split(""); 
     let vowelCount = 0;
-    let arrayOf = ["a", "e", "i", "o", "u", "y"];
+    let arrayOfFunction = ["a", "e", "i", "o", "u"];
     
    for (let i = 0; i < words.length; i++) {
-        if (arrayOf.includes(words[i])) { // Checks if the array includes the vowel
+        if (arrayOfFunction.includes(words[i])) { // Checks if the array includes the vowel
             vowelCount++;
         }
     }
