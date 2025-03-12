8f5cd9d520c490ae5abeadc3b50526f4d117bd40
Isaac <isaac@Isaacs-iMac.local>
2025-03-12 17:02:16 -0400

    towLowerCase Update

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function numberofVowels(str) {
-    let words = str.split(""); 
+    let words = str.toLowerCase().split(""); 
     let vowelCount = 0;
     let arrayOfFunction = ["a", "e", "i", "o", "u"];
     
    for (let i = 0; i < words.length; i++) {
-        if (arrayOfFunction.includes(words[i])) { // Checks if the array includes the vowel
+        if (arrayOfFunction.includes(words[i])) { // Checks if the array includes the vowel "i" goes through every single letter
             vowelCount++;
         }
     }
@@ -13,6 +13,6 @@ function numberofVowels(str) {
 }
 
 
-console.log(numberofVowels("superhero"));
+console.log(numberofVowels("suPErherO"));
 
 
